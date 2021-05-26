cba9c36b29d9a13b282e0d660517c4522f049b30
kafi mannan <mannan.kafi@gmail.com>
2021-05-26 17:03:13 +0600

    image fade

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text,TouchableOpacity , Image, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello React</Text>
+      <TouchableOpacity >
+
+      <Image source={{
+        width: 200,
+        height:300,
+        uri: "https://picsum.photos/200/300"
+      }}></Image>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
